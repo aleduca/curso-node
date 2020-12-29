2c3179f517913fc10e6382325411dd3256780e3c
aleduca <alecar2007@gmail.com>
2020-12-28 23:39:19 -0300

    flash messages

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,22 +1,15 @@
 const express = require("express");
+
 const Home = require("../controllers/Home");
 const Login = require("../controllers/Login");
 const Protected = require("../controllers/Protected");
+const { login: loginValidation } = require("../validations/login");
 
 const router = express.Router();
 
 router.get("/", Home.index);
 router.get("/login", Login.index);
-router.get(
-  "/protected",
-  function (request, response, next) {
-    if (!request.session.name) {
-      return response.redirect("/login");
-    }
-    next();
-  },
-  Protected.index
-);
-router.post("/login", Login.store);
+router.get("/protected", Protected.index);
+router.post("/login", loginValidation, Login.store);
 
 module.exports = router;
